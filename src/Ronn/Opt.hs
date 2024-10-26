f5ecfffbec2fde64aba53d40387e63efb49def6e
patrick brisbin <pbrisbin@gmail.com>
2024-10-26 09:17:51 -0400

    License as AGPLv3

--- src/Ronn/Opt.hs
+++ src/Ronn/Opt.hs
@@ -1,3 +1,11 @@
+-- |
+--
+-- Module      : Ronn.Opt
+-- Copyright   : (c) 2024 Patrick Brisbin
+-- License     : AGPL-3
+-- Maintainer  : pbrisbin@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
 module Ronn.Opt
   ( Opts (..)
   , optsToDefinitions
