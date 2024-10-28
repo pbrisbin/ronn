59b7b01bac6f5b2f3795f78e5a6ad75cb8441ad9
patrick brisbin <pbrisbin@gmail.com>
2024-10-28 10:22:46 -0400

    Add missing headroom comments

--- ronn/tests/Main.hs
+++ ronn/tests/Main.hs
@@ -1,1 +1,9 @@
+-- |
+--
+-- Module      : Main
+-- Copyright   : (c) 2024 Patrick Brisbin
+-- License     : AGPL-3
+-- Maintainer  : pbrisbin@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
 {-# OPTIONS_GHC -F -pgmF hspec-discover -Wno-missing-export-lists #-}
--- ronn/tests/Ronn/ManRefSpec.hs
+++ ronn/tests/Ronn/ManRefSpec.hs
@@ -1,3 +1,11 @@
+-- |
+--
+-- Module      : Ronn.ManRefSpec
+-- Copyright   : (c) 2024 Patrick Brisbin
+-- License     : AGPL-3
+-- Maintainer  : pbrisbin@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
 module Ronn.ManRefSpec
   ( spec
   ) where
