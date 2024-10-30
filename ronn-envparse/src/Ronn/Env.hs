235329a7bf5e4d8714f4465559df37d5dc284caa
patrick brisbin <pbrisbin@gmail.com>
2024-10-30 13:19:41 -0400

    ronn-envparse

---
+++ ronn-envparse/src/Ronn/Env.hs
@@ -0,0 +1,33 @@
+-- |
+--
+-- Module      : Ronn.Env
+-- Copyright   : (c) 2024 Patrick Brisbin
+-- License     : AGPL-3
+-- Maintainer  : pbrisbin@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
+module Ronn.Env
+  ( envDefinitions
+  ) where
+
+import Prelude
+
+import Data.String (IsString (..))
+import Env (Parser)
+import Env.Internal.Help (helpDoc)
+import Ronn.AST
+
+envDefinitions :: Parser e a -> [Definition]
+envDefinitions = map fromHelpLine . drop 2 . lines . helpDoc
+
+fromHelpLine :: String -> Definition
+fromHelpLine x =
+  Definition
+    { name
+    , description = Line [help]
+    , content = Nothing
+    }
+ where
+  (name, help) = case words x of
+    (y : ys) -> (fromString y, fromString $ unwords ys)
+    _ -> ("", "(error: envparse helpline was empty)")
